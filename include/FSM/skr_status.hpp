453ff9b1c5bb8727713284d83cf974bd47f8fc2d
DESKTOP-BDQQ75R\82109 <ghd5135@naver.com>
2026-04-07 11:36:19 +0900

    한글 주석 추가

--- include/FSM/skr_status.hpp
+++ include/FSM/skr_status.hpp
@@ -1,12 +1,12 @@
 
 enum class SKR_STATUS
 {
-    INIT,
-    STANBY,
-    CHECK,
-    SEARCH_READY,
-    SEARCH,
-    TRACK,
-    ERROR
-    
+    INIT, // 초기화
+    STANBY, // 대기
+    CHECK, // 점검
+    SEARCH_READY, // 탐색 준비
+    SEARCH, //  탐색
+    TRACK, // 추적
+    ERROR // 에러
+
 };
