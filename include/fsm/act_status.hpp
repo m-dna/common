c80016e4762c6a5f8edb39c71c825534e602b2af
AnKdy1227 <123557052+AnKdy1227@users.noreply.github.com>
2026-04-07 12:18:55 +0900

    fsm 수정

--- include/fsm/act_status.hpp
+++ include/fsm/act_status.hpp
@@ -1,7 +1,10 @@
-enum class ACT_STATUS{
-    INIT,
-    READY,
-    CHECK,
-    CONTROL,
-    ERROR,
+#ifndef ACT_STATUS_HPP
+#define ACT_STATUS_HPP
+enum class ActStatus{ // ACT_STATUS
+    INIT, // 초기화
+    READY, // 대기
+    CHECK, // 점검
+    CONTROL, // 제어
+    ERROR, // 고장
 };
+#endif// ACT_STATUS_HPP
