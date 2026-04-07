37c1c07c49362854eaa421c6af0dee4086ac29f5
AnKdy1227 <123557052+AnKdy1227@users.noreply.github.com>
2026-04-07 12:22:31 +0900

    format 적용

--- include/dto/pin_control_res.hpp
+++ include/dto/pin_control_res.hpp
@@ -1,8 +1,8 @@
 #ifndef PIN_CONTROL_RES_HPP
 #define PIN_CONTROL_RES_HPP
 #include <stdint.h>
-struct PinControlRes{
-    uint16_t message_id;
+struct PinControlRes {
+  uint16_t message_id;
 };
 
-#endif// PIN_CONTROL_RES_HPP
+#endif // PIN_CONTROL_RES_HPP
--- include/fsm/act_status.hpp
+++ include/fsm/act_status.hpp
@@ -1,10 +1,10 @@
 #ifndef ACT_STATUS_HPP
 #define ACT_STATUS_HPP
-enum class ActStatus{ // ACT_STATUS
-    INIT, // 초기화
-    READY, // 대기
-    CHECK, // 점검
-    CONTROL, // 제어
-    ERROR, // 고장
+enum class ActStatus { // ACT_STATUS
+  INIT,                // 초기화
+  READY,               // 대기
+  CHECK,               // 점검
+  CONTROL,             // 제어
+  ERROR,               // 고장
 };
-#endif// ACT_STATUS_HPP
+#endif // ACT_STATUS_HPP
