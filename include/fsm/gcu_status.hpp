6016e1f949e6e3c3d34e2dc8534f1c513780e53c
남상엽 <spearoad15@gmail.com>
2026-04-07 12:21:15 +0900

    refact : clangd formatting

--- include/fsm/gcu_status.hpp
+++ include/fsm/gcu_status.hpp
@@ -1,7 +1,7 @@
-enum class GCU_STATUS // gcu_status // GCU_STATUS
+enum class GCU_STATUS  // gcu_status // GCU_STATUS
 {
-    INIT,       // 초기화
-    READY,      // 준비 완료
-    RUNNING,    // 실행 중
-    ERROR       // 오류 발생
+  INIT,     // 초기화
+  READY,    // 준비 완료
+  RUNNING,  // 실행 중
+  ERROR     // 오류 발생
 };
