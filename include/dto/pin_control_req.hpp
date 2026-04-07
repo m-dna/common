7ad39be990761318039fa7577a1895e34aff464b
AnKdy1227 <123557052+AnKdy1227@users.noreply.github.com>
2026-04-07 12:16:32 +0900

    feat : act dto 생성

---
+++ include/dto/pin_control_req.hpp
@@ -0,0 +1,10 @@
+#ifndef PIN_CONTROL_REQ_HPP
+#define PIN_CONTROL_REQ_HPP
+#include <stdint.h>
+struct PinControlReq{
+    uint16_t message_id;
+    int32_t a_motor_theta;
+    int32_t b_motor_theta;
+};
+
+#endif// PIN_CONTROL_REQ_HPP
