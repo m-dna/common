12d74f68dbda74a06651d495acaf1ef8f5c01c4e
남상엽 <spearoad15@gmail.com>
2026-04-08 15:31:03 +0900

    fix : act status type 변경(uint8_t -> ActStatus)

--- include/dto/act_info.h
+++ include/dto/act_info.h
@@ -3,9 +3,11 @@
 
 #include <stdint.h>
 
+#include "../fsm/act_status.h"
+
 typedef struct {
   uint16_t message_id;
-  uint8_t act_status;
+  ActStatus act_status;
   int32_t a_motor_theta;
   int32_t a_motor_omega;
   int32_t b_motor_theta;
