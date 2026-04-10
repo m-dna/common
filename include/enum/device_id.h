91a228cb22e3636e280be47f90a2b46020ec8a26
cheoljun99 <cheoljun99@naver.com>
2026-04-10 11:37:35 +0900

    [feat] : device_id enum class 추가

--- include/enum/device_id.h
+++ include/enum/device_id.h
@@ -4,11 +4,12 @@
 #include <cstdint>
 
 enum class DeviceId : uint8_t {
-  GCU = 0x00,  // 유도조종장치
-  ACT = 0x01,  // 액추에이터
-  INS = 0x02,  // 비행제어장치
-  SKR = 0x03,  // 탐색기
-  UI = 0x04    // 사용자 인터페이스
+  GCU = 0x00,     // 유도조종장치
+  ACT = 0x01,     // 액추에이터
+  INS = 0x02,     // 비행제어장치
+  SKR = 0x03,     // 탐색기
+  UI = 0x04       // 사용자 인터페이스
+  INVALID = 0xFF  // 유효하지 않은 장치 ID
 };
 
 #endif  // DEVICE_ID_H
