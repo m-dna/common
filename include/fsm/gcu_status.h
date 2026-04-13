2ebb6187e19910f397792d3f321b622264b7694d
김부건 <kbg8146@gmail.com>
2026-04-13 17:48:52 +0900

    gcu_status에 error 추가

--- include/fsm/gcu_status.h
+++ include/fsm/gcu_status.h
@@ -4,13 +4,14 @@
 #include <cstdint>
 
 enum class GcuStatus : uint8_t {
-  INIT,              // 초기화,
-  READY,             // 대기,
-  CHECK,             // 점검
-  LAUNCH,            // 발사
-  INERTIAL_GUIDANCE, // 관성유도
-  TERMINAL_GUIDANCE, // 종말유도
-  AIRBURST           // 비상폭발
+  INIT,               // 초기화,
+  READY,              // 대기,
+  CHECK,              // 점검
+  LAUNCH,             // 발사
+  INERTIAL_GUIDANCE,  // 관성유도
+  TERMINAL_GUIDANCE,  // 종말유도
+  AIRBURST,           // 비상폭발
+  ERROR               // 고장
 };
 
-#endif // GcuStatus_H
+#endif  // GcuStatus_H
