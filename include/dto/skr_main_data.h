0b296e1bf858f065c72e7dd6805645a4f21030e9
DESKTOP-BDQQ75R\82109 <ghd5135@naver.com>
2026-04-16 13:39:03 +0900

    스프린트2 UC-2-1에 따른 SKR dto 추가 및, 기능 점검에서의 SKR 전송 데이터 변경

---
+++ include/dto/skr_main_data.h
@@ -0,0 +1,16 @@
+#ifndef skr_main_data_H
+#define skr_main_data_H
+
+#include <cstdint>
+
+#include "../enum/icd_id.h"
+
+#pragma pack(push, 1)
+struct SkrMainData {
+  IcdId message_id;
+  int16_t skr_yaw_offset;
+  int16_t skr_pitch_offset;
+};
+#pragma pack(pop)
+
+#endif  // skr_main_data_H
