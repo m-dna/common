f46947e563ca805a6d4452c089a0e83043d5a043
cheoljun99 <cheoljun99@naver.com>
2026-04-18 01:06:44 +0900

    comm_header.h 수정

--- include/protocol/comm_header.h
+++ include/protocol/comm_header.h
@@ -9,7 +9,7 @@ enum class CommType : uint8_t {
   RELIABLE_DATA = 0x02,
   ACK = 0x03
 };
-
+#pragma pack(push, 1)
 struct CommHeader {
   CommType type; // 통신 유형 (일반 데이터, 신뢰성 있는 데이터, ACK)
   DeviceId src_device_id; // 신뢰성 통신을 위한 출발지 디바이스 ID
@@ -19,5 +19,6 @@ struct CommHeader {
   uint16_t total_length;   // 헤더 + 데이터의 총 길이 (최대 65535)
   uint16_t data_length;    // 데이터의 길이 (최대 65535)
 };
+#pragma pack(pop)
 
 #endif // COMM_HEADER_H
