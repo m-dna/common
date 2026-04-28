1478856f3fee2c4ce3d2d0b6f68bb098b199c08e
AnKdy1227 <123557052+AnKdy1227@users.noreply.github.com>
2026-04-28 09:30:15 +0900

    오타 수정 pin -> fin

--- include/dto/one_test_result.h
+++ include/dto/one_test_result.h
@@ -4,7 +4,7 @@
 #include <cstdint>
 
 #include "../enum/icd_id.h"
-#include "pin_control_req.h"
+#include "fin_control_req.h"
 #include "position_res.h"
 #include "target_distance_res.h"
 
@@ -13,7 +13,7 @@ struct OneTestResult {
   IcdId message_id;
   TargetDistanceRes target_distance_res;
   PositionRes position_res;
-  PinControlReq pin_control_req;
+  FinControlReq fin_control_req;
   uint32_t algorithm_time;
   uint32_t act_comm_time;
   uint32_t ins_comm_time;
